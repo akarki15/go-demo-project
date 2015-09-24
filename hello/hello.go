25f95bb04eeda92b65d74be3da4109bb04a2add1
Aashish Karki <aashishkarki@Aashishs-MacBook-Pro.local>
2015-09-24 15:46:48 -0400

    Concurrent input matrix creation works

--- hello/hello.go
+++ hello/hello.go
@@ -5,17 +5,18 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
-	// "strings"
+	"time"
 )
 
 // Creates a matrix of given input size and writes it to a file
-func writeMatrix(width int, height int, fileName string) {
-	fmt.Println("Start")
+func writeMatrix(width int, height int, fileName string, c chan string) {
+	fmt.Println("Creating", fileName)
 	file, err := os.Create(fileName)
 	if err != nil {
 		return
 	}
 	defer file.Close()
+
 	for i := 0; i < height; i++ {
 		numList := ""
 		for j := 0; j < width; j++ {
@@ -23,14 +24,37 @@ func writeMatrix(width int, height int, fileName string) {
 		}
 		file.WriteString(numList + "\n")
 	}
+	c <- "done"
 }
 
+// Spawns two goroutines to create the two input files
 func createMatrices(dim int, fileName1, fileName2 string) {
-	go writeMatrix(dim, dim, fileName1)
-	go writeMatrix(dim, dim, fileName2)
-	fmt.Print("Success!")
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go writeMatrix(dim, dim, fileName1, c1)
+	go writeMatrix(dim, dim, fileName2, c2)
+
+	go func() {
+		for {
+			select {
+			case <-c1:
+				fmt.Println("Done with", fileName1)
+			case <-c2:
+				fmt.Println("Done with", fileName2)
+			default:
+				fmt.Print(".")
+				time.Sleep(time.Second * 3)
+			}
+		}
+	}()
+
+	var input string
+	fmt.Scanln(&input)
+
+	fmt.Print("Stopped!")
 }
 
 func main() {
-	createMatrices(1<<8, "input1", "input2")
+	createMatrices(1<<10, "input1", "input2")
 }
