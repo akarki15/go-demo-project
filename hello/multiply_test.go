f62cbded5f567a1d59c7d8e8b3aa8e73a5f10da1
Aashish Karki <aashishkarki@Aashishs-MacBook-Pro.local>
2015-09-24 16:21:22 -0400

    Cleanup and added test file

---
+++ hello/multiply_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+	"github.com/gonum/matrix/mat64""
+)
+
+func TestMultiply(t *testing.T){
+	cases := [] struct{
+		in, want [][] int
+	}{
+		{read()}, 
+		{},
+	}
+
+}
