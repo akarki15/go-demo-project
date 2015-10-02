7b0c6f540a8331364903af85abe96c43e164de52
Aashish Karki <0133asis@gmail.com>
2015-10-02 10:54:11 -0400

    multiply_test passes

--- matrixMult/multiply_test.go
+++ matrixMult/multiply_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -9,25 +10,29 @@ func TestMultiply(t *testing.T) {
 		in1, in2, want Matrix
 	}{
 		{
-			Matrix{{Element{48}, Element{39}}, {Element{37}, Element{95}}},
-			Matrix{{Element{65}, Element{82}}, {Element{29}, Element{87}}},
-			Matrix{{Element{4251}, Element{7329}}, {Element{5160}, Element{11299}}},
+			Matrix{Val: [][]Element{{Element{48}, Element{39}}, {Element{37}, Element{95}}}},
+			Matrix{Val: [][]Element{{Element{65}, Element{82}}, {Element{29}, Element{87}}}},
+			Matrix{Val: [][]Element{{Element{4251}, Element{7329}}, {Element{5160}, Element{11299}}}},
 		},
 		{
-			Matrix{{Element{23}, Element{32}}, {Element{43}, Element{23}}},
-			Matrix{{Element{89}, Element{84}}, {Element{76}, Element{64}}},
-			Matrix{{Element{4479}, Element{3980}}, {Element{5575}, Element{5084}}},
+			Matrix{Val: [][]Element{{Element{23}, Element{32}}, {Element{43}, Element{23}}}},
+			Matrix{Val: [][]Element{{Element{89}, Element{84}}, {Element{76}, Element{64}}}},
+			Matrix{Val: [][]Element{{Element{4479}, Element{3980}}, {Element{5575}, Element{5084}}}},
 		},
 	}
 
 	for _, c := range cases {
 
 		matChannel := make(chan Matrix)
-		go multiply(c.in1, c.in2, matChannel)
+		go multiply(c.in1, c.in2, matChannel, 0, 0, 0, 0)
 
 		got := <-matChannel
-		if !got.IsEqual(c.want) {
+		ans, err := got.IsEqual(c.want)
+		if err != nil {
+			fmt.Println(err)
+		} else if !ans {
 			t.Errorf("Multiply(%v, %v) == %v, want %v", c.in1, c.in2, got, c.want)
 		}
+
 	}
 }
